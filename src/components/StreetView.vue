92560e3a600069ca59a1df1f03e4a583cc36f0a4
gawema <gab.mannu@hotmail.it>
2021-03-23 18:35:37 +0100

    added streetview map

--- src/components/StreetView.vue
+++ src/components/StreetView.vue
@@ -1,35 +1,56 @@
 <template>
-  <div id="street-view">
-      {{ this.randCoord }}
-  </div>
+  <h1>StreetView.vue</h1>
+  <!-- <div id="street-view">
+    {{ this.randCoord }}
+  </div> -->
+  <div id="map"></div>
+  <div id="pano"></div>
 </template>
 
 <script>
-    import google from 'googleapis'
-    import locations from './../assets/locations'
+// import google from "googleapis";
+import locations from "./../assets/locations";
 
 export default {
-       
-    name: "StreetView",
-    data(){
-        return {
-            googleApiKey: process.env.GOOGLE_API_KEY,
-            randCoord: locations[Math.floor(Math.random() * locations.length)],
+  name: "StreetView",
+  data() {
+    return {
+      //   googleApiKey: process.env.GOOGLE_API_KEY,
+      randCoord: locations[Math.floor(Math.random() * locations.length)],
+    };
+  },
+  mounted() {
+    window.checkAndAttachMapScript(this.createMap);
+  },
+  methods: {
+    createMap() {
+      const fenway = { lat: 42.345573, lng: -71.098326 };
+      const map = new window.google.maps.Map(document.getElementById("map"), {
+        center: fenway,
+        zoom: 14,
+      });
+      const panorama = new window.google.maps.StreetViewPanorama(
+        document.getElementById("pano"),
+        {
+          position: fenway,
+          pov: {
+            heading: 34,
+            pitch: 10,
+          },
         }
+      );
+      map.setStreetView(panorama);
     },
-    methods: {
-        initialize() {
-            console.log(locations.locations);
-            new google.maps.StreetViewPanorama(
-            document.getElementById("street-view"),
-            {
-                position: { lat: this.randCoord[0], lng: this.randCoord[1] },
-                pov: { heading: 165, pitch: 0 },
-                zoom: 1,
-                addressControl: false,
-            }
-        );
-    }
-    }
-}
+  },
+};
 </script>
+<style lang="css">
+/* #map {
+  height: 400px;
+  width: 400px;
+} */
+#pano {
+  height: 400px;
+  width: 400px;
+}
+</style>
